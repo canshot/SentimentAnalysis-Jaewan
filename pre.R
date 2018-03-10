4044cd328e7af7641d9a9e1a27664aafa4113079
Jaewan Lim <31329026+canshot@users.noreply.github.com>
2018-03-09 19:24:42 -0500

    Update pre.R

--- pre.R
+++ pre.R
@@ -45,6 +45,11 @@ ggplot(data.frame(book6), aes(x=formats)) + geom_bar()
 
 #create the row of Hardcover format 
 hard1 = subset(book1, formats == "Hardcover")
+hard2 = subset(book2, formats == "Hardcover")
+hard3 = subset(book3, formats == "Hardcover")
+hard4 = subset(book4, formats == "Hardcover")
+hard5 = subset(book5, formats == "Hardcover")
+hard6 = subset(book6, formats == "Hardcover")
 
 #create the row of Kindle format 
 kindle1 = subset(book1, formats == "Kindle Edition")
@@ -57,5 +62,40 @@ paper1 = subset(book1, formats == "Paperback")
 
 #create the row of Audio CD format 
 audio1 = subset(book1, formats == "Audio CD")
+
+
+#manipulate and order by DATE
+
+# %Y: 4-digit year (1982), %y: 2-digit year (82), %m: 2-digit month (01), %d: 2-digit day of the month (13), 
+# %A: weekday (Wednesday)%a: abbreviated weekday (Wed), %B: month (January), %b: abbreviated month (Jan)
+
+hard1$dates = as.Date(hard1$dates, format = "%B %d, %Y")
+hard2$dates = as.Date(hard2$dates, format = "%d-%b-%y")
+hard3$dates = as.Date(hard3$dates, format = "%d-%b-%y")
+hard4$dates = as.Date(hard4$dates, format = "%d-%b-%y")
+hard6$dates = as.Date(hard6$dates, format = "%d-%b-%y")
+
+#order by date
+library(dplyr)
+hard1=arrange(hard1, dates)
+hard2=arrange(hard2, dates)
+hard3=arrange(hard3, dates)
+hard4=arrange(hard4, dates)
+hard6=arrange(hard6, dates)
+
+#######################task 0309 ends
+
+#character & word count
+library(qdap)
+character_count(hard1$texts)
+sum(character_count(hard1$texts))
+mean(character_count(hard1$texts))
+sd(character_count(hard1$texts))
+plot(character_count(hard1$texts), ylab="character count", main = "[Book1] Character # of Hardcover")
+View(hard1)
+testdate <- hard1[rev(order(as.Date(hard1$dates))),]
+
+plot(wc(hard1$texts))
+sum(wc(hard1$texts))
 
 
